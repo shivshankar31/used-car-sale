ecd0defd9e2a698a9853def623fd109e120dc750
shivshankar31 <shivshankar31@yahoo.com>
2022-03-03 19:44:26 +0530

    15: step 7: install and create postgres DB

--- carzoneproj/settings.py
+++ carzoneproj/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'carzoneproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_DB',
+        'USER' : 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : '127.0.0.1',
+        'PORT' : '5050'
+
     }
 }
 
