aaac3490c00318e4bbec6e802005a66fd3a43d81
shivshankar31 <shivshankar31@yahoo.com>
2022-03-14 06:36:08 +0530

    27, 28: step 15: Car Model and admin panel

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from cars.models import Car
 
 # Register your models here.
+
+
+
+class CarsAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (None, {
+            "fields": ('car_title', 'model', 'color', 'year', 'price', 'city', 'state'),
+        }),
+        ('Body ', {
+            'fields': ('body_style','doors', 'features', 'interior', 'passenger' )
+        }),
+        ('Engine Type ', {
+            'fields': ('engine', 'trasmission', 'vin_no', 'miles', 'millage', 'fuel_type' )
+        }),
+        ( 'condition', {
+            'fields': ( 'description', 'no_of_owners', 'is_featured' )
+        }),
+        ( 'Photo', {
+            # 'classes': ('collapse',), # to ide the below fields
+            'fields': ('car_photo','car_photo1', 'car_photo2', 'car_photo3', 'car_photo4', 'car_photo5' )
+        }),
+    
+    )
+    
+
+
+
+admin.site.register(Car,CarsAdmin)
+
+
+
+
+
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,119 @@
+from random import choices
 from django.db import models
+from datetime import datetime
+# import datetime
 
 # Create your models here.
+
+class Car(models.Model):
+
+    city_choice = (
+        ('Chennai', 'Chennai'),
+        ('Coimbatore', 'Coimbatore' ),
+        ('Madurai', 'Madurai'),
+        ('Salem', 'Salem'),
+        ('Tiruppur', 'Tiruppur'),
+        ('Tiruchirappalli', 'Tiruchirappalli'),
+        ('Vellore', 'Vellore' ),
+        ('Tirunelveli', 'Tirunelveli' ),
+        ('Erode','Erode' ),
+        ('Thoothukkudi', 'Thoothukkudi' ),
+        ('Dindigul', 'Dindigul'),
+        ('Thanjavur', 'Thanjavur'),
+        ('Hosur', 'Hosur'),
+        ('Sivakasi', 'Sivakasi'),
+        ('Karur', 'Karur'),
+        ('Udhagamandalam', 'Udhagamandalam'),
+        ('Ranipet', 'Ranipet'),
+        ('Nagercoil', 'Nagercoil'),
+
+    )
+
+    state_choice = (
+        ('TN', 'TAMIL NADU'),
+        ('other state', 'other state')
+        
+    )
+
+    year_choice = []
+    for r in range(2000, (datetime.now().year+1)):
+        year_choice.append((r,r))
+
+    features_choices = (
+        ('Cruise Control', 'Cruise Control'),
+        ('Audio Interface', 'Audio Interface'),
+        ('Airbags', 'Airbags'),
+        ('Air Conditioning', 'Air Conditioning'),
+        ('Seat Heating', 'Seat Heating'),
+        ('Alarm System', 'Alarm System'),
+        ('ParkAssist', 'ParkAssist'),
+        ('Power Steering', 'Power Steering'),
+        ('Reversing Camera', 'Reversing Camera'),
+        ('Direct Fuel Injection', 'Direct Fuel Injection'),
+        ('Auto Start/Stop', 'Auto Start/Stop'),
+        ('Wind Deflector', 'Wind Deflector'),
+        ('Bluetooth Handset', 'Bluetooth Handset'),
+    )
+
+    body_choice =(
+        ('Hatchback','Hatchback'),
+        ('Sedan', 'Sedan'),
+        ('MUV/SUV', 'MUV/SUV'),
+        ('Coupe','Coupe'),
+        ('Convertible', 'Convertible'),
+        ('Wagon', 'Wagon'),
+        ('Van', 'Van'),
+        ('Jeep','Jeep'),
+    )
+
+    trasmission_choice = (
+        ('Manual', 'Manual'),
+        ('Automatic', 'Automatic'),
+    )
+
+    door_choices = (
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+    )
+
+    fuel_choice = ( 
+        ('Diesel', 'Diesel'),
+        ('Petrol', 'Petrol'), 
+        ('Electric', 'Electric')
+        )
+   
+
+    car_title = models.CharField(max_length=50)
+    city = models.CharField(choices=city_choice, max_length=50)
+    state = models.CharField(choices=state_choice,max_length=50)
+    color = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.IntegerField(choices=year_choice)
+    congition = models.CharField(max_length=50)
+    price = models.IntegerField()
+    description = models.CharField(max_length=100)
+    car_photo = models.ImageField(upload_to= 'photo/%Y/%M/%D')
+    car_photo1 = models.ImageField(upload_to= 'photo/%Y/%M/%D', blank=True)
+    car_photo2 = models.ImageField(upload_to= 'photo/%Y/%M/%D', blank=True)
+    car_photo3 = models.ImageField(upload_to= 'photo/%Y/%M/%D', blank=True)
+    car_photo4 = models.ImageField(upload_to= 'photo/%Y/%M/%D', blank=True)
+    car_photo5 = models.ImageField(upload_to= 'photo/%Y/%M/%D', blank=True)
+    features = models.CharField(choices=features_choices,max_length=50)
+    body_style = models.CharField(choices= body_choice ,max_length=50)
+    engine = models.CharField(max_length=50)
+    trasmission = models.CharField(choices=trasmission_choice, max_length=50)
+    interior = models.CharField(max_length=50)
+    miles = models.IntegerField()
+    doors = models.CharField(choices=door_choices,max_length=10)
+    passenger = models.IntegerField()
+    vin_no = models.CharField(max_length=50)
+    millage = models.IntegerField()
+    fuel_type = models.CharField(choices=fuel_choice,max_length=20)
+    no_of_owners = models.IntegerField()
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateField(auto_now_add=True)
+    
+
