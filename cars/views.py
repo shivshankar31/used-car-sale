39196af415467df1b7fbdcad66173a9fcdd5ecde
shivshankar31 <shivshankar31@yahoo.com>
2022-03-20 06:05:34 +0530

    38: step 20: single car detail page

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from cars.models import Car
 
 # Create your views here.
@@ -14,4 +14,12 @@ def cars(request):
     return render (request, 'cars/cars.html', car)
 
 def car_detail(request, id):
-    return render (request, 'cars/car_detail.html')
+    # detail = Car.objects.all() #normal way without 404 error
+    detail = get_object_or_404(Car,pk=id) # inbuilt function
+
+    detail = {
+        'detail' : detail,
+        
+    }
+
+    return render (request, 'cars/car_detail.html', detail)
