07071664e660bf85654a174d6acacebc5120c156
shivshankar31 <shivshankar31@yahoo.com>
2022-03-27 05:23:28 +0530

    46, 47:step 24: home page search form and display

--- cars/views.py
+++ cars/views.py
@@ -33,10 +33,49 @@ def car_detail(request, id):
 def search(request):
     search_data = Car.objects.order_by('-created_date')
 
+    
+
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
             data = search_data.filter(Q(description__icontains = keyword)|Q(car_title__icontains=keyword))
+
+    
+        
+    # if 'key' in request.GET:
+    #     search_fields = ['year','city','model','body_style']
+    #     for key in search_fields:
+    #         keyword = request.GET[key]
+    #         if keyword:
+    #             data = search_data.filter(keyword__iexact=key)
+
+    if 'year' in request.GET:
+        year = request.GET['year']
+        if year:
+            data = search_data.filter(year__iexact=year)
+    
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            data = search_data.filter(city__iexact=city)
+    
+    if 'model' in request.GET:
+        model = request.GET['model']
+        if model:
+            data = search_data.filter(model__iexact=model)
+    
+    if 'body_style' in request.GET:
+        body_style = request.GET['body_style']
+        if body_style:
+            data = search_data.filter(body_style__iexact=body_style)
+
+    if 'min_price' in request.GET:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        if max_price:
+            data = search_data.filter(price__gte=min_price, price__lte=max_price)
+
+
 
     # data1 = {
     #     'data': search_data,
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
+from tkinter import FLAT
 from django.shortcuts import render
+from cars.views import search
 
 from pages.models import Teams
 from cars.models import Car
@@ -10,12 +12,24 @@ def home(request):
     team = Teams.objects.all()
     featured_car = Car.objects.order_by('-created_date').filter(is_featured=True)
     latest_car = Car.objects.order_by('-created_date')[:6]
-
+    # search_car = Car.objects.values('model','city', 'year', 'body_style', 'price').distinct()
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+    
+    
     # to dislay team members in home and about page 
     empl = {
         'teams' : team,
         'featured_cars': featured_car,
-        'latest_car': latest_car 
+        'latest_car': latest_car,
+        # 'search_car': search_car,
+        'model_search': model_search,
+        'year_search': year_search,
+        'city_search': city_search,
+        'body_style_search': body_style_search,
+        
     }
 
     return render(request, 'pages/home.html',empl)
